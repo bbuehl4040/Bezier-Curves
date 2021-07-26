52849e5c66ac308c65007bf54af0fe6b0b4233f8
bbuehl4040 <billy@expandshare.com>
2021-07-26 13:27:01 -0400

    get_coord recursive function

--- bezier.py
+++ bezier.py
@@ -3,63 +3,47 @@
 
 from matplotlib import pyplot as plt
 
-def get_coord(t, t_start, t_stop, start_val, stop_val, ease_out, ease_in):
-    # single parameter bezier
-    # time on x axis
-    t_prog = 1 / (t_stop - t_start) * (t - t_start)
-    p1 = (t_start, start_val)
-    p4 = (t_stop, stop_val)
-    p2 = ((p1[0] + (p4[0] - p1[0]) * ease_out), start_val)
-    p3 = ((p1[0] + (p4[0] - p1[0]) * (1 - ease_in)), stop_val)
+def get_point(p1, p2, dist):
+    return (1-dist) * p1 + (dist * p2)
+
+def get_coord(t, points=[]):
+    # base case
+    if len(points) < 2:
+        return points
     
-    x = ((1-t_prog)**3 * p1[0]) + (3 * (1-t_prog)**2 * t_prog * p2[0]) + (3 * (1-t_prog) * t_prog**2 * p3[0]) + (t_prog**3 * p4[0])
-    y = ((1-t_prog)**3 * p1[1]) + (3 * (1-t_prog)**2 * t_prog * p2[1]) + (3 * (1-t_prog) * t_prog**2 * p3[1]) + (t_prog**3 * p4[1])
+    # find midpoints between points
+    midpoints = []
+    i_0 = 0
+    i_1 = 1
+    while i_1 < len(points):
+        m_x = get_point(points[i_0][0], points[i_1][0], t)
+        m_y = get_point(points[i_0][1], points[i_1][1], t)
+        midpoints.append((m_x, m_y))
+        i_0 += 1
+        i_1 += 1
     
-    print(f'x: {x}, y: {y}, t_prog: {t_prog}, p1: {p1}, p2: {p2}, p3: {p3}, p4: {p4}')
+    point = get_coord(t, midpoints)
 
-    return (x,y)
+    return point
 
-def get_coord2(t, p1, p2):
-    x = ((1-t) * p1[0]) + (t * p2[0])
-    y = ((1-t) * p1[1]) + (t * p2[1])
-
-    return int(x), int(y)
-
-def get_coord4(t, p1, p2, p3, p4):
-    x = ((1-t)**3 * p1[0]) + (3 * (1-t)**2 * t * p2[0]) + (3 * (1-t) * t**2 * p3[0]) + (t**3 * p4[0])
-    y = ((1-t)**3 * p1[1]) + (3 * (1-t)**2 * t * p2[1]) + (3 * (1-t) * t**2 * p3[1]) + (t**3 * p4[1])
-
-    return round(x,2), round(y,2)
-    
-def easeCustom(t, t_start, t_stop, p1, p4, ease_out, ease_in):
-    # 1/duration * (distance from animation start)
-    t_trans = 1 / (t_stop-t_start) * (t - t_start)
 
 if __name__ == '__main__':
     print('\noutput: \n')
-    positions = 30
+
+    points = [(0,0), (3.33,0), (0, 10), (10,10)]
     x_positions = []
     y_positions = []
+    times = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
 
-    # animation starts at 2s and goes to 7s
-    times = [2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7]
     for t in times:
-        x, y = get_coord(t, 2, 7, 0, 10, .4, 1)
-        x_positions.append(x)
-        y_positions.append(y)
-
-    # for i in range(positions+1):
-    #     t = i / positions
-    #     # x, y = get_coord4(t, (3,10), (10,0), (0,0), (7,10))
-    #     # x, y = get_coord4(t, (0,0), (3,0), (0,10), (10,10))
-    #     x, y = get_coord2(t, (0,0), (10,10))
-    #     x_positions.append(x)
-    #     y_positions.append(y)
+        coord = get_coord(t, points)[0]
+        x_positions.append(coord[0])
+        y_positions.append(coord[1])
 
     plt.plot(x_positions, y_positions)
     plt.title("stupid curve")
-    plt.xlabel = "Time"
-    plt.ylabel = "Parameter"
+    plt.xlabel = "X"
+    plt.ylabel = "Y"
     plt.show()
 
     print('\ndone.')
